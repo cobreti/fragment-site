ac7857a088159024853c4d4cbf18b593f7df444c
dannyt <cobreti_@hotmail.com>
2021-09-15 12:51:33 -0400

    initial commit

---
+++ src/fragment-element.ts
@@ -0,0 +1,40 @@
+class FragmentElement extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const nameAttr = this.getAttribute('name');
+
+        const iframe = document.createElement('iframe');
+        iframe.onload = () => {
+            console.log('iframe loaded');
+        }
+
+        const srcAttr = this.getAttribute('src');
+        if (srcAttr) {
+            iframe.setAttribute('src', srcAttr);
+        }
+
+        const classesAttr = this.getAttribute('class');
+        if (classesAttr) {
+            iframe.setAttribute('class', classesAttr);
+        }
+
+        let p = document.createElement('p');
+        this.appendChild(iframe);
+        this.appendChild(p);
+    }
+
+    disconnectedCallback() {
+
+    }
+
+    attributeChangedCallback(name: string, oldValue: any, newValue: any) {
+
+    }
+
+    adoptedCallback() {
+
+    }
+}
